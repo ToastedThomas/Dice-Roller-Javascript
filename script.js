797d9daa4881746227dfa4cbc6e36613f83eb866
ToastedThomas <96932052+ToastedThomas@users.noreply.github.com>
2022-05-11 01:24:04 -0500

    Added stats and new dice

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const diceButtons = [document.getElementById('d6'), 
-document.getElementById('d2'), ]
-const availableDice = ['d6', 'd2']
+const diceButtons = [document.getElementById('d6'), document.getElementById('d2'), document.getElementById('d8'), document.getElementById('d10'), document.getElementById('d12') ]
+const availableDice = ['d6', 'd2', 'd8', 'd10', 'd12']
 const rollDiceButton = document.getElementById('diceRoll');
 const reRollButton = document.getElementById('reRollButton');
 const homeButton = document.getElementById('menuButton');
@@ -16,18 +15,7 @@ element.onclick = function() {
     diceSelect(index);
   };
 })
-rollDiceButton.onclick = function() {
-  hideMenu();
-  clearTable();
-  rollTheDice();
-}
-reRollButton.onclick = function() { rollDiceButton.click(); }
-homeButton.onclick = function() { 
-  clearTable();
-  document.getElementById('afterRollMenu').style.display = 'none';
-  document.getElementById('mainMenu').style.display = 'flex';
-  document.getElementById('diceTable').style.display = 'none';
-}
+
 function diceSelect(index) {
   document.getElementById('selectedDice').innerHTML = availableDice[index];
   diceType = availableDice[index]
@@ -39,6 +27,7 @@ function hideMenu() {
 }
 function rollTheDice() {
   var numOfDice = document.getElementsByTagName("input")[0].value;
+  var rollTotal = 0;
   for (var i = 0; i < numOfDice; i++) {
     var img = document.createElement('img');
     var div = document.createElement('div');
@@ -54,8 +43,10 @@ function rollTheDice() {
   rolls.forEach((element, index) => {
     element = randomNum();
     console.log(element);
+    rollTotal += element;
     document.getElementById(index).src = 'images/'+diceType+'_'+element+'.png';
   })
+  statsUpdate(rollTotal);
 }
 function clearTable() {
   rolls = [0];
@@ -69,5 +60,31 @@ function randomNum() {
     return (Math.floor(Math.random() * 6) + 1);
   } else if (diceType == 'd2') {
     return (Math.floor(Math.random() * 2) + 1);
+  } else if (diceType == 'd8') {
+    return (Math.floor(Math.random() * 8) + 1);
+  } else if (diceType == 'd10') {
+    return (Math.floor(Math.random() * 10) + 1);
+  } else if (diceType == 'd12') {
+    return (Math.floor(Math.random() * 12) + 1);
   }
+}
+function statsUpdate(rollTotal) {
+  document.getElementById('rolledDice').innerHTML = rolls.length;
+  document.getElementById('rollTotal').innerHTML = rollTotal;
+  document.getElementById('averageRoll').innerHTML = (rollTotal / rolls.length).toFixed(1);
+}
+rollDiceButton.onclick = function() {
+  hideMenu();
+  clearTable();
+  rollTheDice();
+}
+reRollButton.onclick = function() { 
+  clearTable();
+  rollTheDice();
+}
+homeButton.onclick = function() { 
+  clearTable();
+  document.getElementById('afterRollMenu').style.display = 'none';
+  document.getElementById('mainMenu').style.display = 'flex';
+  document.getElementById('diceTable').style.display = 'none';
 }
